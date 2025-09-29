a51272f3a32da1e6d5f0d9f26f94ac3184d34355
fllipeis <p.eistrach@gmail.com>
2025-09-29 20:05:05 +0200

    update: api version [skip ci]

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
     `maven-publish`
 }
 
-val baseVersion = "0.0.17"
+val baseVersion = "0.0.18"
 val commitHash = System.getenv("COMMIT_HASH")
 val timestamp = System.currentTimeMillis()
 val snapshotVersion = "${baseVersion}-dev.${timestamp}-${commitHash}"
