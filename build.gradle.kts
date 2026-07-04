a0986331b3bd56118f63017e5bf3052555e42b47
fllipeis <p.eistrach@gmail.com>
2026-07-04 18:16:49 +0200

    chore: bump baseVersion to 0.1.0-platform.32 in build script

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
     `signing`
 }
 
-val baseVersion = "0.1.0-platform.31"
+val baseVersion = "0.1.0-platform.32"
 val commitHash = System.getenv("COMMIT_HASH")
 val isSnapshot = commitHash != null
 
