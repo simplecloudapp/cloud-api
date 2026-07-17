572a53210234e3df0e4a115b1dd18a86d4ee1e65
fllipeis <p.eistrach@gmail.com>
2026-07-17 19:19:27 +0200

    chore: bump baseVersion to 0.1.0-platform.41

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
     `signing`
 }
 
-val baseVersion = "0.1.0-platform.40"
+val baseVersion = "0.1.0-platform.41"
 val commitHash = System.getenv("COMMIT_HASH")
 val isSnapshot = commitHash != null
 
